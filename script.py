2a39e0b6ec8788deaf801d205dbbe19c384499f9
Niels <niels@nielsdimmers.nl>
2023-02-18 22:58:24 +0100

    2023-12-18 (59) new script.py and mailcheck cleanup

---
+++ script.py
@@ -0,0 +1,26 @@
+# script.py
+import requests
+import notion_interface
+import urllib.parse
+import sys
+from Nconfig import config
+from global_vars import global_vars
+import log
+
+config = config.config()
+notion = notion_interface.notion()
+log = log.log()
+
+def log_count():
+	requests.get(global_vars.TELEGRAM_MSG_URL % (config.get_item('telegram','TELEGRAM_API_TOKEN'),config.get_item('telegram','TELEGRAM_CHAT_ID'),urllib.parse.quote(log.get_size_message())))
+
+def daily():
+	requests.get(global_vars.TELEGRAM_MSG_URL % (config.get_item('telegram','TELEGRAM_API_TOKEN'),config.get_item('telegram','TELEGRAM_CHAT_ID'),urllib.parse.quote(notion.get_daily_data())))
+	
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print(global_vars.SCRIPT_USAGE)
+	elif sys.argv[1] == 'daily':
+		daily()
+	elif sys.argv[1] == 'logcount':
+		log_count()
